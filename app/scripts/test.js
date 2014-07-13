ef1fe8904fd6b72a8128ff598f0720819fe02087
mo-ahmed <mo.hassan19@gmail.com>
2014-07-13 14:56:04 +0200

    added screen share test elements

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -5,4 +5,9 @@ $(document).ready(function () {
     $('.checklist-row').toggle();
     $('.test-wrapper').toggle();
     });
+  $('.real-start-test').click(function () {
+    $('.test-notes').hide();
+    $('.test-title').text("Screen sharing");
+    $('.test-description').text("Let's check if we can see your screen. That's it, you can sit back and relax! We got it from here. The test will take less than a minute.");
+    });
 });
