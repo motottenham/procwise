247f234eb340a35ce7b2bbe363106e727c5024d2
mo-ahmed <mo.hassan19@gmail.com>
2014-07-14 10:48:32 +0200

    commit for commit sake

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -5,9 +5,20 @@ $(document).ready(function () {
     $('.checklist-row').toggle();
     $('.test-wrapper').toggle();
     });
-  $('.real-start-test').click(function () {
-    $('.test-notes').hide();
+  $('#start-test').click(function () {
+    $(this).toggleClass("hide");
+    $('#screen-notworking').toggleClass("hide");
+    $('.test-notes').empty().append('<div class="screen-feed"></div>');
     $('.test-title').text("Screen sharing");
-    $('.test-description').text("Let's check if we can see your screen. That's it, you can sit back and relax! We got it from here. The test will take less than a minute.");
+    $('.test-description').text("Let's check if we can see your screen.");
+    $('.test-progress').show();
+    });
+   $('#screen-notworking').click(function () {
+    $(this).toggleClass("hide");
+    $('#mic-notworking').toggleClass("hide");
+    $('#screen-check').toggleClass("hide");
+    $('.test-notes').empty().append('<h1>Say Something</h1>');
+    $('.test-title').text("Microphone");
+    $('.test-description').text("Let's check if your microphone works.");
     });
 });
